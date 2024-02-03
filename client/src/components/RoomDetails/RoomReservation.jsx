fc8f7e57b61d7c84912d24838d777e5ae57f72c6
jahangir <jahangiralomtuf99@gmail.com>
2024-02-03 12:40:09 +0600

    added songthing

--- client/src/components/RoomDetails/RoomReservation.jsx
+++ client/src/components/RoomDetails/RoomReservation.jsx
@@ -6,20 +6,20 @@ const RoomReservation = ({ room }) => {
   return (
     <div className="border-[1px] p-5 rounded-xl overflow-hidden border-neutral-200 bg-white">
       <div className="flex flex-row items-center gap-1 p-4">
-      <div className='text-2xl font-semibold'>$ {room.price}</div>
-        <div className='font-light text-neutral-600'>night</div>
+        <div className="text-2xl font-semibold">$ {room.price}</div>
+        <div className="font-light text-neutral-600">night</div> <hr />
       </div>
-          <div className="justify-center">
-          <Calender />
-          </div>
-          <hr />
-      <div className='p-4'>
-        <Button label='Reserve' />
+      <div className="justify-center">
+        <Calender />
       </div>
       <hr />
-      <div className='p-4 flex flex-row items-center justify-between font-semibold text-lg'>
-              <div>Total </div>
-              <div> $ {room.price}</div>
+      <div className="p-4">
+        <Button label="Reserve" />
+      </div>
+      <hr />
+      <div className="p-4 flex flex-row items-center justify-between font-semibold text-lg">
+        <div>Total </div>
+        <div> $ {room.price}</div>
       </div>
     </div>
   );
